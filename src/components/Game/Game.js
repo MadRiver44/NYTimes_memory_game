939696ff9cba28a05c7fb6e4ee97ba4154841ece
Kevin Turney <super996@optonline.net>
2018-04-09 16:14:28 -0400

    add Instructions component

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import Instructions from '../Instructions/Instructions'
+import Button from '../Button/Button'
 
-import Timer from '../Timer/Timer'
+// import Timer from '../Timer/Timer'
 import styles from './Game.scss'
 
 const Game = () => (
   <div>
-    <h1 className={styles.header}>NYT Games Code Test</h1>
-    <Timer />
-    <div className={styles.placeholder}>Let the games begin (here).</div>
+    <h1 className={styles.header}>Welcome to the NY Times Memory Game</h1>
+    <Instructions />
+    <div className={styles.buttonContainer}>
+      <Button>Easy</Button>
+      <Button>Hard</Button>
+    </div>
   </div>
 )
 
---
+++ src/components/Instructions/Instructions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Instructions.scss'
+
+const Instructions = () => (
+  <div>
+    <div className={styles.instructions}>Instuctions.</div>
+    <ol className={styles.orderedList}>
+      <li>The cards will appear face down</li>
+      <li>Select a card</li>
+      <li>After first card is selected, try to match it</li>
+      <li>If two cards match, they will be removed from the board</li>
+      <li>When there are only two cards left, the game is won!</li>
+    </ol>
+  </div>
+)
+export default Instructions
