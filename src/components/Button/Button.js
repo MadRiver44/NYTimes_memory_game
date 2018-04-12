9e71acf0607c83a123bb94aa854208a6ef692dcf
Kevin Turney <super996@optonline.net>
2018-04-12 19:44:50 -0400

    create Button, todo validate props

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import Cards from '../Cards/Cards'
+// import PropTypes from 'prop-types'
 import styles from './Button.scss'
 
+// const Button = props => (
+//   <div className={styles.container}>
+//     <button className={styles.button}>{props.children}</button>
+//   </div>
+// )
+
+// removed this. from props
 const Button = props => (
   <div className={styles.container}>
-    <button className={styles.button}>{props.children}</button>
+    <button className={styles.button} onClick={props.onClick}>
+      {props.diffculty}
+    </button>
   </div>
 )
 
 export default Button
 
-Button.propTypes = {
-  children: PropTypes.element.isRequired,
-}
+// validate props difficulty and onClick
