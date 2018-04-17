d40fca201ab29d522f56f1e1748e5a54521653c3
Kevin Turney <super996@optonline.net>
2018-04-17 09:57:45 -0400

    add isCountingTime, change Button prop name, remove getData var, lint errors persist

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,40 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import PropTypes from 'prop-types'
 import Instructions from '../Instructions/Instructions'
 import Timer from '../Timer/Timer'
 import Cards from '../Cards/Cards'
 import Button from '../Button/Button'
-import axios from 'axios'
-
-// import Timer from '../Timer/Timer'
 import styles from './Game.scss'
-
-// const Game = () => (
-//   <div>
-//     <h1 className={styles.header}>Welcome to the NY Times Memory Game</h1>
-//     <Instructions />
-//     <div className={styles.buttonContainer}>
-//       <Button>Easy</Button>
-//       <Button>Hard</Button>
-//     </div>
-//   </div>
-// )
 
 class Game extends Component {
   constructor(props) {
     super(props)
-
     this.state = {
       easyButton: '',
       hardButton: '',
       easyCards: [],
       hardCards: [],
       cardTypes: [],
+      currentCards: [],
+      itemsArray: [],
     }
     this.handleEasyCards = this.handleEasyCards.bind(this)
     this.handleHardCards = this.handleHardCards.bind(this)
+    this.removeMatches = this.removeMatches.bind(this)
   }
+
   componentDidMount() {
-    const getData = axios
+    axios
       .get('https://web-code-test-dot-nyt-games-prd.appspot.com/cards.json')
       .then(response => {
         this.setState({ easyButton: response.data.levels[0].difficulty })
@@ -46,27 +37,48 @@ class Game extends Component {
         console.log(error)
       })
   }
+
   handleEasyCards() {
-    this.setState({ currentCards: this.state.easyCards })
+    this.setState({ currentCards: this.state.easyCards, isCountingTime: true })
   }
 
   handleHardCards() {
-    this.setState({ currentCards: this.state.hardCards })
+    this.setState({ currentCards: this.state.hardCards, isCountingTime: true })
   }
+
+  toggleHidden() {
+    this.setState({ isHidden: !this.state.isHidden })
+  }
+
+  removeMatches(items) {
+    if (items.length === 1) {
+      this.setState({ itemsArray: [...items] })
+    }
+    if (items[0] === this.state.itemsArray[0]) {
+      this.setState(prevState => ({
+        currentCards: prevState.currentCards.filter(card => items.indexOf(card) === -1),
+      }))
+    } // else
+  }
+
   render() {
     return (
-      <div>
+      <div style={styles.boardContainer}>
+        {this.state.isCountingTime && <Timer />}
         <h1 className={styles.header}>Welcome to the NY Times Memory Game</h1>
         <Instructions />
         <div className={styles.buttonContainer}>
-          <Timer />
-          <Button difficulty={this.state.easyButton} onClick={this.handleEasyCards} />
-          <Button difficulty={this.state.hardButton} onClick={this.handleHardCards} />
+          <Button difficulty={this.state.easyButton} clickHandler={this.handleEasyCards} />
+          <Button difficulty={this.state.hardButton} clickHandler={this.handleHardCards} />
         </div>
-        <Card cardTypes={this.state.currentCards} />
+        <Cards cardTypes={this.state.currentCards} removeMatches={this.removeMatches} />
       </div>
     )
   }
 }
 
 export default Game
+
+Game.PropTypes = {
+  removeMatches: PropTypes.func,
+}
